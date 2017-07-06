3912ab2744381db9f423515cf732ef87ab1e1fa1
Gilberto Marcon <gilbertoamarcon@gmail.com>
2017-07-05 20:41:59 -0700

    Pioneer launch script fixed

--- scripts/pioneer-navigation.sh
+++ scripts/pioneer-navigation.sh
@@ -28,6 +28,12 @@ pid="$! $pid"
 
 sleep 3s
 
+echo "Launching robot description..."
+roslaunch pioneer_test pioneer_description.launch &
+pid="$! $pid"
+
+sleep 2s
+
 trap "echo Killing all processes.; kill -2 $pid; exit" SIGINT SIGTERM
 
 sleep 24h
