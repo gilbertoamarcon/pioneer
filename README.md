c01f1aec1ff7ea05e4ac1f7c36c141cf9c09db1b
Gilberto Marcon <gilbertoamarcon@gmail.com>
2017-07-05 19:37:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# box
+
+Simplified version of the Pioneer Packages from [a link](https://github.com/JenJenChung).
+
+I kept the minimal necessary for navigation using a Adept MobileRobots Pioneer Robot. 
+
+All of the code is based on packages from [a link](https://github.com/JenJenChung).
+
+Dependencies: 
+
+ROSARIA [a link](http://wiki.ros.org/ROSARIA).
+
+pioneer_test [a link](https://github.com/JenJenChung/pioneer_test).
+
+On a pioneer, run:
+
+```rosrun pioneer pioneer-navigation.sh```
+
+[Optional] On a desktop computer, run:
+
+```roslaunch pioneer teleop_ps3.launch```
---
+++ scripts/pioneer-navigation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Launching roscore..."
+roscore &
+pid=$!
+sleep 5s
+
+echo "Connecting to RosAria..."
+rosrun rosaria RosAria &
+pid="$! $pid"
+sleep 3s
+
+echo "Launching sensors..."
+roslaunch pioneer_test pioneer_sensors.launch &
+pid="$! $pid"
+
+sleep 3s
+
+echo "Launching navigation stack..."
+roslaunch pioneer navigation.launch map_name:=$(rospack find pioneer)/maps/printer_000.yaml &
+pid="$! $pid"
+
+sleep 3s
+
+echo "Launching pioneer controller..."
+roslaunch pioneer_test pioneer_controller_spin_recover.launch &
+pid="$! $pid"
+
+sleep 3s
+
+trap "echo Killing all processes.; kill -2 $pid; exit" SIGINT SIGTERM
+
+sleep 24h
